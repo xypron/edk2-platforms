xypron/edk2-platforms

commit 6daebc9af276422a3aee3d2204b4731d4b517e29

RISC-V: Implement RISCV_EFI_BOOT_PROTOCOL
RISC-V platforms need to support new RISCV_EFI_BOOT_PROTOCOL
to communicate the boot hart id to the OS. This patch
implements this protocol.

Signed-off-by: Sunil V L <sunilvl@ventanamicro.com>

Silicon/RISC-V/ProcessorPkg/Universal/CpuDxe/CpuDxe.c
@@ -7,6 +7,9 @@
 
 **/
 
+#include <Library/BaseMemoryLib.h>
+#include <RiscVImpl.h>
+#include <Library/RiscVFirmwareContextLib.h>
 #include "CpuDxe.h"
 
 //
@@ -14,6 +17,27 @@
 //
 STATIC BOOLEAN     mInterruptState = FALSE;
 STATIC EFI_HANDLE  mCpuHandle      = NULL;
+STATIC UINTN mBootHartId;
+
+EFI_STATUS
+EFIAPI
+RiscvGetBootHartId (
+  IN RISCV_EFI_BOOT_PROTOCOL   *This,
+  OUT UINTN                    *BootHartId
+  )
+{
+  if((This == NULL) || (BootHartId == NULL)) {
+    return EFI_INVALID_PARAMETER;
+  }
+
+  *BootHartId = mBootHartId;
+  return EFI_SUCCESS;
+}
+
+RISCV_EFI_BOOT_PROTOCOL  gRiscvBootProtocol = {
+  RISCV_EFI_BOOT_PROTOCOL_LATEST_VERSION,
+  RiscvGetBootHartId
+};
 
 EFI_CPU_ARCH_PROTOCOL  gCpu = {
   CpuFlushCpuDataCache,
@@ -285,6 +309,18 @@ InitializeCpu (
   )
 {
   EFI_STATUS  Status;
+  EFI_RISCV_OPENSBI_FIRMWARE_CONTEXT *FirmwareContext;
+
+  GetFirmwareContextPointer (&FirmwareContext);
+  ASSERT (FirmwareContext != NULL);
+  if (FirmwareContext == NULL) {
+    DEBUG ((DEBUG_ERROR, "Failed to get the pointer of EFI_RISCV_OPENSBI_FIRMWARE_CONTEXT\n"));
+    return EFI_NOT_FOUND;
+  }
+  DEBUG ((DEBUG_INFO, " %a: Firmware Context is at 0x%x.\n", __FUNCTION__, FirmwareContext));
+
+  mBootHartId = FirmwareContext->BootHartId;
+  DEBUG ((DEBUG_INFO, " %a: mBootHartId = 0x%x.\n", __FUNCTION__, mBootHartId));
 
   //
   // Machine mode handler is initiated in CpuExceptionHandlerLibConstructor in
@@ -296,6 +332,13 @@ InitializeCpu (
   //
   DisableInterrupts ();
 
+  Status = gBS->InstallProtocolInterface (&ImageHandle,
+				          &gUefiRiscVBootProtocolGuid,
+					  EFI_NATIVE_INTERFACE,
+					  &gRiscvBootProtocol
+					 );
+
+  ASSERT_EFI_ERROR (Status);
   //
   // Install CPU Architectural Protocol
   //

Platform/Qemu/RiscvVirt/RiscvVirt.dsc
@@ -292,6 +292,7 @@
   VariablePolicyLib|MdeModulePkg/Library/VariablePolicyLib/VariablePolicyLib.inf
 
 [LibraryClasses.common.DXE_DRIVER]
+  RiscVFirmwareContextLib|Silicon/RISC-V/ProcessorPkg/Library/RiscVFirmwareContextSscratchLib/RiscVFirmwareContextSscratchLib.inf
   PcdLib|MdePkg/Library/DxePcdLib/DxePcdLib.inf
   TimerLib|Silicon/RISC-V/ProcessorPkg/Library/RiscVTimerLib/BaseRiscVTimerLib.inf
   HobLib|MdePkg/Library/DxeHobLib/DxeHobLib.inf

Silicon/RISC-V/ProcessorPkg/Include/RiscVImpl.h
@@ -27,6 +27,8 @@ typedef UINT64 RISC_V_REGS_PROTOTYPE;
 #else
 #endif
 
+typedef struct _RISCV_EFI_BOOT_PROTOCOL RISCV_EFI_BOOT_PROTOCOL;
+
 //
 // Structure for 128-bit value
 //
@@ -83,5 +85,20 @@ typedef struct _RISCV_MACHINE_MODE_CONTEXT {
   EFI_PHYSICAL_ADDRESS    UserModeTrapHandler;       /// USer mode trap handler.
   TRAP_HANDLER_CONTEXT    MModeHandler;              /// Handler for machine mode.
 } RISCV_MACHINE_MODE_CONTEXT;
+
+#define RISCV_EFI_BOOT_PROTOCOL_REVISION 0x00010000
+#define RISCV_EFI_BOOT_PROTOCOL_LATEST_VERSION \
+	RISCV_EFI_BOOT_PROTOCOL_REVISION
+
+typedef EFI_STATUS
+(EFIAPI *EFI_GET_BOOT_HARTID) (
+  IN RISCV_EFI_BOOT_PROTOCOL   *This,
+  OUT UINTN		       *BootHartId
+  );
+
+typedef struct _RISCV_EFI_BOOT_PROTOCOL {
+  UINT64                Revision;
+  EFI_GET_BOOT_HARTID   GetBootHartId;
+} RISCV_EFI_BOOT_PROTOCOL;
 
 #endif

Silicon/RISC-V/ProcessorPkg/Library/RiscVFirmwareContextSscratchLib/RiscVFirmwareContextSscratchLib.inf
@@ -15,7 +15,7 @@
   FILE_GUID                      = 3709E048-6794-427A-B728-BFE3FFD6D461
   MODULE_TYPE                    = PEIM
   VERSION_STRING                 = 1.0
-  LIBRARY_CLASS                  = RiscVFirmwareContextLib|PEIM PEI_CORE
+  LIBRARY_CLASS                  = RiscVFirmwareContextLib|PEIM PEI_CORE DXE_DRIVER
 
 #
 #  VALID_ARCHITECTURES           = RISCV64

Silicon/RISC-V/ProcessorPkg/RiscVProcessorPkg.dec
@@ -31,6 +31,9 @@
 [Guids]
   gUefiRiscVPkgTokenSpaceGuid  = { 0x4261e9c8, 0x52c0, 0x4b34, { 0x85, 0x3d, 0x48, 0x46, 0xea, 0xd3, 0xb7, 0x2c}}
 
+[Protocols]
+  gUefiRiscVBootProtocolGuid   = { 0xccd15fec, 0x6f73, 0x4eec, { 0x83, 0x95, 0x3e, 0x69, 0xe4, 0xb9, 0x40, 0xbf}}
+
 [PcdsFixedAtBuild]
   # Processor Specific Data GUID HOB GUID
   gUefiRiscVPkgTokenSpaceGuid.PcdProcessorSpecificDataGuidHobGuid|{0x20, 0x72, 0xD5, 0x2F, 0xCF, 0x3C, 0x4C, 0xBC, 0xB1, 0x65, 0x94, 0x90, 0xDC, 0xF2, 0xFA, 0x93}|VOID*|0x00001000

Silicon/RISC-V/ProcessorPkg/Universal/CpuDxe/CpuDxe.inf
@@ -20,6 +20,7 @@
   MdeModulePkg/MdeModulePkg.dec
   MdePkg/MdePkg.dec
   Silicon/RISC-V/ProcessorPkg/RiscVProcessorPkg.dec
+  Platform/RISC-V/PlatformPkg/RiscVPlatformPkg.dec
 
 [LibraryClasses]
   BaseLib
@@ -31,6 +32,7 @@
   TimerLib
   UefiBootServicesTableLib
   UefiDriverEntryPoint
+  RiscVFirmwareContextLib
 
 [Sources]
   CpuDxe.c
@@ -38,6 +40,7 @@
 
 [Protocols]
   gEfiCpuArchProtocolGuid                       ## PRODUCES
+  gUefiRiscVBootProtocolGuid                    ## PRODUCES
 
 [Pcd]
   gUefiRiscVPkgTokenSpaceGuid.PcdRiscVMachineTimerFrequencyInHerz

commit 5f699fa0dd33ffedeab07591ed5a7e0e121fcf9a

RISC-V: fix RISCV_EFI_BOOT_PROTOCOL
The proposed RISC-V boot protocol specification requires that calling
GetBootHartId() with a value of This other than the protocol interface
address shall result in EFI_INVALID_PARAMETER. It is not sufficient
to test that This is not NULL.

Cf. https://bugzilla.tianocore.org/show_bug.cgi?id=3837

Fixes: 6daebc9af276 ("RISC-V: Implement RISCV_EFI_BOOT_PROTOCOL")
Signed-off-by: Heinrich Schuchardt <heinrich.schuchardt@canonical.com>

Silicon/RISC-V/ProcessorPkg/Universal/CpuDxe/CpuDxe.c
@@ -18,6 +18,7 @@
 STATIC BOOLEAN     mInterruptState = FALSE;
 STATIC EFI_HANDLE  mCpuHandle      = NULL;
 STATIC UINTN mBootHartId;
+RISCV_EFI_BOOT_PROTOCOL gRiscvBootProtocol;
 
 EFI_STATUS
 EFIAPI
@@ -26,7 +27,7 @@ RiscvGetBootHartId (
   OUT UINTN                    *BootHartId
   )
 {
-  if((This == NULL) || (BootHartId == NULL)) {
+  if((This != &gRiscvBootProtocol) || (BootHartId == NULL)) {
     return EFI_INVALID_PARAMETER;
   }
 